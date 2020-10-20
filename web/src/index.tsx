8fe2bd10de26e2af0040fd7987508d68530ba470
Saenyakorn Siangsanoh <saneyakorn@gmail.com>
2020-10-20 13:26:36 +0700

    refactor: file name

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -2,7 +2,7 @@ import React from "react"
 import ReactDOM from "react-dom"
 
 import "./theme/index.css"
-import AppModule from "./modules/app"
+import AppModule from "./modules/app.module"
 import { register } from "./serviceWorker"
 
 ReactDOM.render(<AppModule />, document.getElementById("root"))
